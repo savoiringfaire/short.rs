0013cf35e99e9a1237febd54a745a01a5b349ec2
Marcus Hann <marcus@hhra.me>
2019-09-23 13:35:38 +0100

    Add rudimentary redis support

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ pub mod short;
 
 use short::Short;
 use futures::{future, Future};
+use std::env;
+use redis::Commands;
 use std::error::Error;
 use simple_error::SimpleError;
 use std::sync::{RwLock, Arc};
@@ -33,23 +35,13 @@ lazy_static! {
 
 type Shorts = Arc<RwLock<Vec<Short>>>;
 
-fn add_short(t: Short) {
-    let shorts = Arc::clone(&SHORTS);
-    let mut lock = shorts.write().unwrap();
-    lock.push(t);
+fn add_short(t: Short, con: &mut redis::Connection) -> redis::RedisResult<()> {
+    let _ : () = con.set(t.token, t.target)?;
+    Ok(())
 }
 
-fn get_target(token: String) -> String {
-    let shorts = Arc::clone(&SHORTS);
-    let lock = shorts.read().unwrap();
-
-    for short in &(*lock) {
-        if short.token == token {
-            return short.target.clone()
-        }
-    }
-
-    return "".to_string()
+fn get_target(token: String, con: &mut redis::Connection) -> redis::RedisResult<String> {
+    con.get(token)
 }
 
 fn get_new() -> ResponseFuture {
@@ -89,12 +81,13 @@ fn get_complete(req: Request<Body>) -> Result<ResponseFuture, Box<dyn Error>> {
     )))
 }
 
-fn post_new(req: Request<Body>) -> Result<ResponseFuture, Box<dyn Error>> {
+fn post_new(req: Request<Body>, redis_client: &Arc<redis::Client>) -> Result<ResponseFuture, Box<dyn Error>> {
+    let mut con = redis_client.get_connection()?;
     let target = get_argument_from_url(req, "target")?;
     let short = Short::new(target)?;
     let token = short.token.clone();
 
-    add_short(short);
+    add_short(short, &mut con)?;
 
     Ok(Box::new(future::ok(
         Response::builder()
@@ -106,16 +99,17 @@ fn post_new(req: Request<Body>) -> Result<ResponseFuture, Box<dyn Error>> {
 }
 
 /// Handle a request that does't match other requests (and therefore should be a redirect request).
-fn get_redirect(req: Request<Body>) -> ResponseFuture {
-    Box::new(future::ok(
+fn get_redirect(req: Request<Body>, redis_client: &Arc<redis::Client>) -> Result<ResponseFuture, Box<dyn Error>> {
+    let mut con = redis_client.get_connection()?;
+    Ok(Box::new(future::ok(
         Response::builder()
             .status(StatusCode::MOVED_PERMANENTLY)
             .header("Location", format!("{}", get_target(
-                req.uri().path()[1..].to_string()))
+                req.uri().path()[1..].to_string(), &mut con)?)
             )
             .body(Body::from(""))
             .unwrap(),
-    ))
+    )))
 }
 
 fn render_error_page(error: Box<dyn Error>) -> ResponseFuture {
@@ -139,19 +133,19 @@ fn respond_handle_error(result: Result<ResponseFuture, Box<dyn Error>>) -> Respo
     }
 }
 
-fn router(req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFuture {
+fn router(req: Request<Body>, _client: &Client<HttpConnector>, redis_client: &Arc<redis::Client>) -> ResponseFuture {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
             get_new()
         }
         (&Method::GET, "/new") => {
-            respond_handle_error(post_new(req))
+            respond_handle_error(post_new(req, redis_client))
         }
         (&Method::GET, "/complete") => {
             respond_handle_error(get_complete(req))
         }
         _ => {
-            get_redirect(req)
+            respond_handle_error(get_redirect(req, redis_client))
         }
     }
 }
@@ -165,11 +159,20 @@ fn main() {
         // create a Client for all Services
         let client = Client::new();
 
+        let connection_string: &str = &env::var("REDIS_CONNECTION_STRING").unwrap();
+
+        let redis_client = Arc::new(
+            redis::Client::open(
+                connection_string
+            ).unwrap()
+        );
+
         // define a service containing the router function
         let new_service = move || {
             // Move a clone of Client into the service_fn
             let client = client.clone();
-            service_fn(move |req| router(req, &client))
+            let redis_client = redis_client.clone();
+            service_fn(move |req| router(req, &client, &redis_client))
         };
 
         // Define the server - this is what the future_lazy() we're building will resolve to
